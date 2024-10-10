5d2e64cd9726d26192ea9c0d4c4671ff43131878
Rejda <qati.rejda@gmail.com>
2024-10-10 14:44:24 +0200

    deployment

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|',
-            'role' => 'required|string|min:8|'
+            'role' => 'required|string|'
         ]);
 
         if ($validator->fails()) {
